1f13a44371ca0936a9a60dc6a924b2d0ec35c3e6
Joern Richter <joerich1988@gmail.com>
2021-09-01 13:06:24 +0200

    Test eventhandler and bump version to 0.1.1

--- showcase_3d_printer_anomaly_counter/eventhandler/EventHandler.py
+++ showcase_3d_printer_anomaly_counter/eventhandler/EventHandler.py
@@ -1,7 +1,7 @@
 import time
 import traceback
 
-from request import Request
+from .request import Request
 
 
 class EventHandler:
@@ -9,15 +9,15 @@ class EventHandler:
     def __init__(self, return_func):
         self.return_func = return_func
         self.request = Request(round(time.time() * 1000.0))
-        self.return_func(self.request)
+        self.return_func(self.request.__dict__)
         self.temp = None
 
-    def on_event(self, event: dict, topic: str):
+    def on_event(self, event: dict, kafka_topic: str):
         try:
-            if topic == "showcase-3d-printer_prusa-esp32":
+            if kafka_topic == "showcase-3d-printer_prusa-esp32":
                 self._handle_raw_data(event)
-            elif topic == "ender_printer_anomalies":
-                self._handle_anomaly_data(event)
+            elif kafka_topic == "ender_printer_anomalies":
+                self._handle_anomaly_data()
         except Exception as e:
             traceback.print_tb(e)
 
@@ -25,9 +25,9 @@ class EventHandler:
         if event['tags']['topic'] == "showcase-3d-printer/prusa-printer/temperature":
             self._handle_raw_temperature_data(event['fields'])
 
-    def _handle_anomaly_data(self, event: dict):
+    def _handle_anomaly_data(self):
         self.request.anomaly_detected()
-        self.return_func(self.request)
+        self.return_func(self.request.__dict__)
 
     def _handle_raw_temperature_data(self, fields: dict):
         if not self.temp:
@@ -37,6 +37,6 @@ class EventHandler:
 
     def _start_new_request(self, timestamp: int):
         self.request.stop_request(timestamp)
-        self.return_func(self.request)
+        self.return_func(self.request.__dict__)
         self.request = Request(timestamp)
-        self.return_func(self.request)
+        self.return_func(self.request.__dict__)
